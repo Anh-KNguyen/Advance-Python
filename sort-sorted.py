2b215468dfaa87db17f53097c8c3f81a2ab38850
Annnh3 <anhknguyen97@yahoo.com>
2020-04-05 21:43:55 -0700

    Under the hood of sorted/sort

--- sort-sorted.py
+++ sort-sorted.py
@@ -1,12 +1,24 @@
 # sorts items in place
 # changes hard copy
 class List:
-    def sort(key=None, reverse=False):
+    def sort(key=None, reverse=False) -> None:
+        # do quicksort with key, reverse if reverse == True
         pass
+
+a_list = ['2','1','3']
+def sorted(iterable):
+    lst = list(iterable)
+    lst.sort() # modifes lst in place
+
+    # return lst
+
+    # convert to generator object, can be converted to list
+    for i in lst:
+        yield i
 
 # returns a copy
 # sorts iterable passed in
 # you cannot "change" an iterable
 
 # sorted(iterable, *, key=None, reverse=False)
-sorted(iterable)
+print(list(sorted(a_list)))
