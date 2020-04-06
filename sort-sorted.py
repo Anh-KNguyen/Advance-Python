ceaaa4ef8ba5842e54bcf9d849230276fee5935d
Annnh3 <anhknguyen97@yahoo.com>
2020-04-05 19:14:14 -0700

    Difference between sort() and sorted()

---
+++ sort-sorted.py
@@ -0,0 +1,12 @@
+# sorts items in place
+# changes hard copy
+class List:
+    def sort(key=None, reverse=False):
+        pass
+
+# returns a copy
+# sorts iterable passed in
+# you cannot "change" a hard copy of an iterable
+
+# sorted(iterable, *, key=None, reverse=False)
+sorted(iterable)
